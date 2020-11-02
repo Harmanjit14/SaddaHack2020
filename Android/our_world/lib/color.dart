bbb48b9cdd57316321532cc3f18dd99bbb4f09e4
Harmanjit Singh <harmanjit140500@gmail.com>
2020-11-02 14:02:57 +0530

    basic project made

---
+++ Android/our_world/lib/color.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class ColorFile{
+
+  final darkBrown = Color(0xff362226);
+
+}
---
+++ Android/our_world/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:our_world/color.dart';
+import 'package:our_world/loginScreen.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget with ColorFile{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Our World',
+      theme: ThemeData.light(),
+      home: Screen1(),
+    );
+  }
+}
